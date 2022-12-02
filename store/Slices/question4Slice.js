2ce9946fb3d0de9298793be3c760fab75fa90fb5
Ayoub <Ayoubrezaei79@gmail.com>
2022-12-02 03:48:31 +0330

    adding question4 redux

--- store/Slices/question4Slice.js
+++ store/Slices/question4Slice.js
@@ -1,16 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  players: [],
+};
 
 export const question4Slice = createSlice({
   name: "questionFour",
   initialState,
   reducers: {
-    // Action to set the authentication status
-    // setAuthState(state, action) {
-    //   state.authState = action.payload;
-    // },
+    savePlayer: (state, { payload }) => {
+      state.players.push({
+        ...payload,
+        id: new Date().getMilliseconds().toString(),
+      });
+    },
+    deletePlayer: (state, { payload }) => {
+      state.players = state.players.filter((player) => player.id !== payload);
+    },
+    updatePlayer: (state, { payload }) => {
+      state.players = state.players.map((player) => {
+        if (player.id === payload.id) {
+          return payload;
+        } else {
+          return player;
+        }
+      });
+    },
   },
 });
+
+export const { savePlayer, deletePlayer, updatePlayer } =
+  question4Slice.actions;
 
 export default question4Slice.reducer;
